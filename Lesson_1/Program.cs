81db66733493f4f5d8a6809c29917d0d75533cdc
Irzhik07 <irina07kuznet@outlook.com>
2024-09-23 21:10:35 +0300

    1
    Упражнение 1. Ввод ФИО

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exs_1_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Введите Ваше Имя");
+                string? firstName = Console.ReadLine();
+
+                Console.WriteLine("Введите Вашу Фамилию");
+                string? lastName = Console.ReadLine();
+
+                Console.WriteLine("Введите Ваше Отчество");
+                string? patrName = Console.ReadLine();
+
+                Console.Write($"{firstName} {lastName} {patrName}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
