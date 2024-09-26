aa133b197c673e9c3a22fa1bbdedc31c048f3221
Irzhik07 <irina07kuznet@outlook.com>
2024-09-26 23:00:12 +0300

    3

---
+++ Lesson_2_Arifm_mean/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lesson_2_Arifm_mean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            
+            try
+            {
+                Console.WriteLine("Введите последовательно пять чисел:");
+                numbers[0] = int.Parse(Console.ReadLine());
+                numbers[1] = int.Parse(Console.ReadLine());
+                numbers[2] = int.Parse(Console.ReadLine());
+                numbers[3] = int.Parse(Console.ReadLine());
+                numbers[4] = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Ошибка: введены некорретные данные \nВыполните повторный ввод данных");
+            }
+            finally
+            {
+                double arifm = (numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]) / 5.0;
+                Console.WriteLine($"Среднее арифметическое введенных чисел = {arifm}");;
+            }
+        }
+    }
+}
---
+++ Lesson_2_Catalog/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Lesson_2_Catalog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] users = new string[2, 3];
+            int[] age = new int[3];
+            try 
+            {
+                Console.WriteLine("Введите данные первого пользователя (1)");
+                Console.Write("Фамилия:");
+                users[0,0] = Console.ReadLine();
+                
+                if (users[0, 0] == "")
+                    throw new ArgumentException("Введены незначащие данные");
+
+                Console.Write("Город:");
+                users[1, 0] = Console.ReadLine();
+                
+                if (users[1, 0] == "")
+                    throw new ArgumentException("Введены незначащие данные");
+
+                Console.Write("Возраст:");
+                age[0] = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            try
+            {
+                Console.WriteLine("Введите данные второго пользователя (2)");
+                Console.Write("Фамилия:");
+                users[0, 1] = Console.ReadLine();
+
+                if (users[0, 1] == "")
+                    throw new ArgumentException("Введены незначащие данные");
+                    
+                Console.Write("Город:");
+                users[1, 1] = Console.ReadLine();
+
+                if (users[1, 1] == "")
+                    throw new ArgumentException("Введены незначащие данные");
+
+                Console.Write("Возраст:");
+                age[1] = int.Parse(Console.ReadLine());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            try
+            {
+                Console.WriteLine("Введите данные третьего пользователя (3)");
+                Console.Write("Фамилия:");
+                users[0, 2] = Console.ReadLine();
+
+                if (users[0, 2] == "")
+                    throw new ArgumentException("Введены незначащие данные");
+
+                Console.Write("Город:");
+                users[1, 2] = Console.ReadLine();
+
+                if (users[1, 2] == "")
+                    throw new ArgumentException("Введены незначащие данные");
+
+                Console.Write("Возраст:");
+                age[2] = int.Parse(Console.ReadLine());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+            try
+            {
+                Console.WriteLine("Выберите пользователя, по которому требуется вывести информацию: 1,2 или 3");
+                string? input = Console.ReadLine();
+                if (input == "1")
+                {
+                    Console.WriteLine($"Результат: Фамилия - {users[0, 0]}, Город - {users[1, 0]}, Возраст - {age[0]}");
+                }
+                else
+                {
+                    Console.WriteLine(input == "2" ? $"Результат: Фамилия - {users[0, 1]}, Город - {users[1, 1]}, Возраст - {age[1]}" : $"Результат: Фамилия - {users[0, 2]}, Город - {users[1, 2]}, Возраст - {age[2]}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
